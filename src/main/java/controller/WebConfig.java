61bdb28dffd0cfb898e2ea70614cb49ce5376743
ThisWillGowell <Shadow89gw>
2016-10-24 13:50:31 -0400

    code formatting cleaned up

--- src/main/java/controller/WebConfig.java
+++ src/main/java/controller/WebConfig.java
@@ -1,15 +1,9 @@
 package controller;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebMvc
