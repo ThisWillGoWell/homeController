e868f77d892b9ecc3ac37259295d355e31e3b0a0
ThisWillGowell <Shadow89gw>
2016-10-29 16:56:30 -0400

    update no longer abstract

--- src/main/java/system/SystemParent.java
+++ src/main/java/system/SystemParent.java
@@ -1,23 +1,56 @@
 package system;
 
+import com.google.gson.JsonObject;
 import controller.Engine;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 /**
  * Created by Willi on 9/26/2016.
  */
+
+
 public abstract class SystemParent {
 
-    protected Engine e;
+    private Engine engine;
+    private long updateInterval;
+    private long lastUpdateTime;
+
     public SystemParent(Engine e)
     {
-        this.e = e;
+        this(e,0);
+    }
+    public SystemParent(Engine e, long updateInterval)
+    {
+        this.updateInterval = updateInterval;
+        this.engine = e;
     }
 
     public  abstract Object get(String what, Map<String, String> requestParams);
     public abstract String set(String what, String to, Map<String, String> requestParams);
 
-    public abstract String getStateJSON();
-    public abstract void update();
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public long getUpdateInterval()
+    {
+        return updateInterval;
+    }
+
+    public long getLastUpdateTime()
+    {
+        return lastUpdateTime;
+    }
+    public void setLastUpdateTime(long l)
+    {
+        lastUpdateTime = l;
+    }
+
+    public void update(){
+
+    }
 }
