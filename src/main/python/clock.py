ed8e59cd1811413ee8ab5828b5c410ecfb535411
ThisWillGowell <Shadow89gw>
2016-10-29 17:05:07 -0400

    Smooth animations now possible

--- src/main/python/clock.py
+++ src/main/python/clock.py
@@ -3,23 +3,27 @@ ONPI = True
 
 import time
 
-import requests #http://docs.python-requests.org/en/master/
 import json
 import math
 import random
-
+import threading
+import requests #http://docs.python-requests.org/en/master/
+import grequests
 
 if ONPI:
     from rgbmatrix import Adafruit_RGBmatrix
     import Image #http://effbot.org/imagingbook/pil-index.htm
     import ImageSequence
     import ImageDraw
-    import thread
+
+
 else:
     from PIL import Image #http://effbot.org/imagingbook/pil-index.htm
     from PIL import ImageSequence
-    from PIL import ImageDraw
-    import threading as thread
+    from PIL import ImageDraw   
+
+
+
 from socket import timeout
 
 
@@ -113,13 +117,13 @@ def processImage(path):
         pass
 
 
-queryTime = 2
+queryTime = 5
 nextStart = math.floor(time.time()) * 1000  
-nextStop = nextStart  + 2000
+nextStop = nextStart  + queryTime * 1000
 nextTimelineUpdateTime = nextStart
 nextImageUpdateTime = nextStart
 interval = 100
-canUpdate= True
+rs = []
 lostConnection = True
 imageTimeline   = []
 imageTimeline= {'interval' :100}
@@ -128,39 +132,55 @@ blankImage = Image.new("RGBA",(96,32), (0,0,0,255))
 layers = {};
 masks = {};
 fills = {};
+successResponse = False;
+processTime = 2
+drawInterval = 100
+im = blankImage
+def exception_handler(request, exception):
+    global lostConnection, reloadImage, successResponse    
+    #print("fail!")
+    successResponse = False
+    lostConnection = True
+    reloadImage = True
 
-def getImageUpdate():
-    global imageTimeline, nextTimelineUpdateTime, nextStart, nextStop, canUpdate, lostConnection, queryTime, interval, reloadImage
-    if time.time() * 1000 > nextTimelineUpdateTime and canUpdate:
-        if reloadImage:
-            url = SERVER_ADDRESS + SERVER_ENDPOINTS[1]
-        else:
-            url = SERVER_ADDRESS + SERVER_ENDPOINTS[0]
-
-        try:
-            print(url)
-            response = requests.get(url + "&t1=%d&t2=%d&interval=%d" % (nextStart, nextStop, interval), timeout = 5)
-            imageTimeline = response.json();
-            print(response.text)
-            canUpdate=False
-            lostConnection = False
-            reloadImage = False
-        except requests.exceptions.Timeout:
-            lostConnection = True
-            reloadImage = True
-        
-        nextTimelineUpdateTime = nextStop - imageTimeline['interval'] * 2
-        nextStart = nextTimelineUpdateTime
-        nextStop = nextTimelineUpdateTime + queryTime * 1000
+def processResponse():
+    global lostConnection, rs, successResponse, imageTimeline, reloadImage, timerCounter
+    #print("process!")
+    if successResponse:
+        timerCounter = timerCounter + time.time()
+        imageTimeline = rs[0].response.json();
+        #print(str(len(rs[0].response.text) / 1024 )+ "kB")
+        lostConnection = False
+        reloadImage = False
 
 
 
-redImage = Image.new("RGBA",(96,32), "red")
+
+def sendRequest():
+    global  queryTime, interval, reloadImage, rs, successResponse, processTime, drawInterval,timerCounter
+    t = long(math.floor(time.time()*1000))
+    timerCounter = time.time()
+    startTime = t-t%drawInterval
+    if reloadImage:     
+        url = SERVER_ADDRESS + SERVER_ENDPOINTS[1] + "&t1=%d&t2=%d&interval=%d" % (startTime , startTime +  (processTime + queryTime + 1)*1000 , drawInterval)
+    else:
+        url = SERVER_ADDRESS + SERVER_ENDPOINTS[0] + "&t1=%d&t2=%d&interval=%d" % (startTime, startTime +  (processTime + queryTime + 1)*1000 , drawInterval)
+    successResponse = True
+    rs = [grequests.get(url ,timeout=processTime)]
+    threading.Timer(3,processResponse).start()
+    threading.Timer(queryTime, sendRequest).start()
+    grequests.map(rs,exception_handler=exception_handler)
+    timerCounter = time.time() - timerCounter
+    
 
 def imageToMask(image, frameNumber=-1):
+    global masks
     #take in a resouce image and return a mask of that image.
     #Store that mask in a dict refrenced by source frame number 
     #so dont have to regenerate it each time.
+    if frameNumber in masks:
+        return masks[frameNumber]
+
     rows = image.size[1]
     cols = image.size[0] 
 
@@ -175,21 +195,15 @@ def imageToMask(image, frameNumber=-1):
         for p in pixels:
             if(p[0] == p[1] == p[2] == 0):
                 m.append(0x00)
-                m.append(0x00)
-                m.append(0x00)
-                m.append(0x00)
             else:
-                m.append(0x00)
-                m.append(0x00)
-                m.append(0x00)
                 m.append(0xff)
         maskData = bytearray(m)
 
 
     else:
         maskData = bytes([])
-        maskOn = bytes([0xff,0x00,0xff,0xff])
-        maskOff = bytes([0x00,0x00,0x00,0x00])
+        maskOn = bytes([0xff])
+        maskOff = bytes([0x00])
         
         for p in range(0, len(pixels)):
             if pixels[p][0] == pixels[p][1] == pixels[p][2] == 0:
@@ -198,32 +212,41 @@ def imageToMask(image, frameNumber=-1):
                 maskData = maskData + maskOn;
 
 
-    mask = Image.frombuffer("RGBA", image.size, maskData)
+    mask = Image.frombuffer("L", image.size, maskData)
     mask = mask.transpose(Image.ROTATE_180).transpose(Image.FLIP_LEFT_RIGHT)
+
+    if frameNumber != -1:
+        masks[frameNumber] = mask
 
     return mask
 
+def putImage():
+    global im
+    threading.Timer(0.1,putImage).start()
+    im.load()
+    matrix.SetImage(im.im.id,0,0)
+    threading.Thread(target=updateImage).start()
 
+
+lastDrawTime = 0
 def updateImage():
-    global layers, blankImage, fills, nextImageUpdateTime, imageTimeline, canUpdate, lostConnection
+    global layers, blankImage, fills, nextImageUpdateTime, imageTimeline, lostConnection, masks,drawInterval, lastDrawTime, last,im
     currentFrame = None
+    start = time.time()
+    #print("starting Draw")
     if lostConnection == True:
-        im = random.choice(resources)
-        im.load()
-        if ONPI:
-            matrix.SetImage(im.im.id, 0, 0)
-        time.sleep(0.5)
-       
-        nextImageUpdateTime = math.floor(time.time()) * 1000  
+        im = random.choice(resources)       
     else:
         currentImageTimeline    = imageTimeline 
-        if time.time() * 1000 >= nextImageUpdateTime:
-            for frame in currentImageTimeline["frames"]:  
-                if len(frame["elements"])!=0:
-                    if frame["time"] - (math.floor(time.time() * 1000 /currentImageTimeline["interval"]) * currentImageTimeline["interval"]) == 0:
-                        currentFrame = frame["elements"]                  
-                        break
-
+        for frame in currentImageTimeline["frames"]:  
+            if len(frame["elements"])!=0:
+                t = math.floor(time.time() * 1000)
+                currentTime = t - t%drawInterval
+                #print(time.time(), (math.floor(time.time() * 1000 /currentImageTimeline["interval"]) * currentImageTimeline["interval"]), frame["time"] - (math.floor(time.time() * 1000 /currentImageTimeline["interval"]) * currentImageTimeline["interval"]),currentImageTimeline["interval"] )
+                if frame["time"] - currentTime == 0:
+                    currentFrame = frame["elements"]                 
+                    break
+                
         if currentFrame != None:
             #im = Image.new("RGBA", resources[0].size)
             for element in currentFrame:
@@ -231,56 +254,60 @@ def updateImage():
                 if element["fill"]["fill"]:
                     key = "%d%d%d%d" % (element["fill"]["r"],element["fill"]["g"],element["fill"]["b"],element["fill"]["a"])
                     if key not in fills:
-                        fills[key] = Image.new("RGBA", (96,32), (element["fill"]["r"],element["fill"]["g"],element["fill"]["b"],element["fill"]["a"]))
+                        fills[key] = Image.new("RGBA", (96,32),(element["fill"]["r"],element["fill"]["g"],element["fill"]["b"],element["fill"]["a"]))
 
                     fillImage = fills[key]
-                    for frame in element["f"]:  
-                        currentLayer.paste(fillImage.crop((0,0,frame["w"], frame["h"])), (frame["c"], frame["r"]),imageToMask(resources[frame["n"]].crop((0,0,frame["w"], frame["h"])), frame["n"]))
+                    for frame in element["f"]:
+                        sr = 0 
+                        sc = 0 
+                        if("sr" in frame):
+                            sr = frame["sr"]
+                        if("sc" in frame):
+                            sc = frame["sc"]
+                        currentLayer.paste(fillImage.crop((sc,sr, sc + frame["w"], sr + frame["h"])), (frame["c"], frame["r"]),imageToMask(resources[frame["n"]].crop((sc,sr,sc + frame["w"], sr + frame["h"]))))
                 else:
                     for frame in element["f"]:
-                        currentLayer.paste(resources[frame["n"]].crop((0,0,frame["w"], frame["h"])), (frame["c"], frame["r"]), imageToMask(resources[frame["n"]].crop((0,0,frame["w"], frame["h"])),frame["n"] ))
+                        sr = 0 
+                        sc = 0 
+                        if("sr" in frame):
+                            sr = frame["sr"]
+                        if("sc" in frame):
+                            sc = frame["sc"]
+
+                        drawImage = resources[frame["n"]].crop((sc,sr,sc + frame["w"], sr + frame["h"]))
+                        currentLayer.paste(drawImage, (frame["c"], frame["r"]), imageToMask(drawImage))
                 layers[element["l"]] = currentLayer
+                masks["layer-"+str(element["l"])] = imageToMask(currentLayer)
             
             im = Image.new("RGBA", (96,32))
+          
             for i in range(len(layers)):
-                im.paste(layers[i],(0,0),imageToMask(layers[i]))
-
+                im.paste(layers[i],(0,0),imageToMask(layers[i],"layer-" + str(i)))
+        
             #im = Image.blend(im, blankImage, currentImageTimeline["alpha"])
+            """
             if ONPI:
-                im.load()
+                #im.load()
                 matrix.SetImage(im.im.id,0,0);
+                print(time.time() - start, time.time() - lastDrawTime,  currentTime)
+                lastDrawTime = time.time()
             else:
                 pass
                 #showImage(image)
-
-            nextImageUpdateTime = nextImageUpdateTime + imageTimeline["interval"]
-    canUpdate = True
-
-def showImage(image):
-    largeImage = image.resize((image.size[0]*10, image.size[1] * 10))
-    largeImage.load()
-    largeImage.show()
+            """
 
 
-def updateThread(threadName):
-    while True :
-        getImageUpdate()
-        time.sleep(imageTimeline["interval"]/1000.0/2)
+
+ 
 
 processImage("resources.gif")
 imageToMask(resources[0], 0)
 start = time.time() * 1000
-if ONPI:
-    thread.start_new_thread(updateThread, ("Update",))
-else:
-    t = thread.Thread(target=updateThread, args=("update",))
-    t.start()
-
 im = Image.new("RGBA", resources[0].size)
 for i in range(10):
     layers[i] = blankImage
+t1 = threading.Timer(0,sendRequest)
+t2 = threading.Timer(3,putImage)
 
-while(True  ):
-    updateImage()
-    time.sleep(0.01)
-
+t1.start()
+t2.start()
